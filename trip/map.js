7933e4d44b7c33273d6d4c288e2764080355c2d2
Jeffrey Meyers <meyers2@pdx.edu>
2013-09-07 17:02:12 -0700

    playing with popups

--- trip/map.js
+++ trip/map.js
@@ -3,10 +3,10 @@ function initmap() {
   map = new L.Map('map');
 
   // create the tile layer with correct attribution
-  //var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  var mapboxUrl='http://a.tiles.mapbox.com/v3/meyersj.map-6u6rh54c/{z}/{x}/{y}.png';
+  //var tileUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var tileUrl='http://a.tiles.mapbox.com/v3/meyersj.map-6u6rh54c/{z}/{x}/{y}.png';
   var attrib='Map data &copy; OpenStreetMap contributors, Designs &copy; Mapbox';
-  var tiles = new L.TileLayer(mapboxUrl, {minZoom: 8, maxZoom: 20, attribution: attrib});	 	
+  var tiles = new L.TileLayer(tileUrl, {minZoom: 8, maxZoom: 20, attribution: attrib});	 	
 
   // Portland
   map.setView(new L.LatLng(45.524, -122.675),12);
@@ -49,3 +49,4 @@ L.NumberedDivIcon = L.Icon.extend({
     return null;
   }
 });
+
