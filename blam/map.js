37e850f924476d9981a2ef20d7c417e3c62d75ce
Jeffrey Meyers <meyers2@pdx.edu>
2013-11-27 19:59:43 -0800

    changes

--- blam/map.js
+++ blam/map.js
@@ -6,10 +6,10 @@ function initmap() {
   //var tileUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var tileUrl='http://a.tiles.mapbox.com/v3/meyersj.map-6u6rh54c/{z}/{x}/{y}.png';
   var attrib='Map data &copy; OpenStreetMap contributors, Designs &copy; Mapbox';
-  var tiles = new L.TileLayer(tileUrl, {minZoom: 10, maxZoom: 20, attribution: attrib});	 	
+  var tiles = new L.TileLayer(tileUrl, {minZoom: 5, maxZoom: 20, attribution: attrib});	 	
 
   // Portland
-  map.setView(new L.LatLng(45.524, -122.675), 17);
+  map.setView(new L.LatLng(45.524, -122.675), 13);
   map.addLayer(tiles);
 }
 
